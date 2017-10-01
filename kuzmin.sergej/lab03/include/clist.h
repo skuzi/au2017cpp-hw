06b9ee093b08a29bd0a975c1b9acde7148141895
kuzmin.sergej <kuzmin.sergej@a12908ea-3afd-459a-82ef-25d554c7e142>
2017-10-01 21:30:41 +0000

    update lab03 version 2.0

--- kuzmin.sergej/lab03/include/clist.h
+++ kuzmin.sergej/lab03/include/clist.h
@@ -1,87 +1,27 @@
 #pragma once
-#include <stdio.h>
+
+#include <stddef.h>
+#include <stdlib.h>
+
 #define container_of(ptr, type, member) (type*)((char*)(ptr) - offsetof(type, member))
 
-//REALIZATION OF INTRUSIVE LIST
-
 struct intrusive_node{
-	struct intrusive_node *next = NULL;
-	struct intrusive_node *prev = NULL;
+	struct intrusive_node* next;
+	struct intrusive_node* prev;
 };
 
-
-struct position_node{
-	int x, y;
-	intrusive_node *node;
-};
-
-
-typedef struct intrusive_list{
-	struct intrusive_node *head = NULL;
-	int size = 0;
-	void add_node(struct intrusive_node *node){
-		if(head != NULL){
-			node->prev = head;
-			node->next = NULL;
-			head->next = node;
-		}
-		head = node;
-	}
-
-
-	void add(int x, int y){
-		struct position_node *new_node = malloc(sizeof(struct position_node));
-		new_node->x = x;
-		new_node->y = y;
-		add_node(&new_node->node);
-		size++;
-	}
-
-
-	void remove(int x, int y){
-		struct intrusive_node *last = head;
-		while(last != NULL){
-			struct position_node *item = container_of(last, position_node, intrusive_node);
-			if(item->x == x && item->y == y){
-				if(last->prev != NULL){
-					last->prev->next = last->next;
-					last->next->prev = last->prev;
-				}
-				else{
-					last->prev->next = NULL;
-					head = last->prev; 
-				}
-				last = last->prev;
-				size--;
-				free(item);
-			}
-		}
-	}
-
-
-	void print(){
-		struct intrusive_node *last = head;
-		while(last != NULL){
-			struct position_node *item = container_of(last, position_node, intrusive_node);
-			printf("(%d %d) ", item->x, item->y);
-			last = last->prev;
-		}
-		printf("\n");
-	}
-
-
-	void clear(){
-		while(head != NULL){
-			struct intrusive_node *last = head->prev;
-			free(container_of(head, position_node, intrusive_node));
-			head = last;
-		}
-		size = 0;
-	}
-
-
-	int len(){
-		return size;
-	}
-
+typedef struct intrusive_list
+{
+	struct intrusive_node* head;
+	int size;
 } clist;
+
+clist* init_list();
+
+void add_node(clist* c, struct intrusive_node* node);
+
+void remove_node(clist* c, struct intrusive_node** node);
+	
+void clear(clist* c);
+
+int get_length(clist* c);
