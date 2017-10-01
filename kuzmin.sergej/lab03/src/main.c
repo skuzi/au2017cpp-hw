1040a6dd382fd9d58c92b5331fbd3f8e0a0a8090
kuzmin.sergej <kuzmin.sergej@a12908ea-3afd-459a-82ef-25d554c7e142>
2017-10-01 21:27:11 +0000

    update lab03

--- kuzmin.sergej/lab03/src/main.c
+++ kuzmin.sergej/lab03/src/main.c
@@ -1,12 +1,60 @@
 #include "clist.h"
-#include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 
-int main() {
-	clist *s = malloc(sizeof(clist));
+struct position_node {
+	int x, y;
+	struct intrusive_node node;
+};
+
+
+void add(clist* s, int x, int y) {
+	struct position_node* new_node = malloc(sizeof(struct position_node));
+	new_node->x = x;
+	new_node->y = y;
+	add_node(s, &new_node->node);
+	s->size++;
+}
+
+
+void rm(clist* s, int x, int y) {
+	struct intrusive_node* last = s->head;
+	while (last != NULL) {
+		struct position_node* item = container_of(last, struct position_node, node);
+		if (item->x == x && item->y == y) {
+			remove_node(s, &last);
+			free(item);
+		} else
+			last = last->prev;
+	}
+	if (s->size == 0)
+		s->head = NULL;	
+}
+
+
+void print(clist* s) {
+	struct intrusive_node* last = s->head;
+	while (last != NULL) {
+		struct position_node* item = container_of(last, struct position_node, node);
+		printf("(%d %d) ", item->x, item->y);
+		last = last->prev;
+	}
+	printf("\n");
+}
+
+void clear(clist* s) {
+	while (s->head != NULL) {
+		struct intrusive_node* last = s->head->prev;
+		free(container_of(s->head, struct position_node, node));
+		s->head = last;
+	}
 	s->size = 0;
 	s->head = NULL;
-	char *command = (char*)malloc(sizeof(char) * 100);
+}
+
+int main() {
+	clist* s = init_list();
+	char* command = (char*)malloc(sizeof(char)*  100);
 	while (1) {
 		if(scanf("%s", command));
 		if (strcmp(command, "add") == 0) {
@@ -15,6 +63,7 @@ int main() {
 			add(s, x, y);
 		}
 		else if (strcmp(command, "rm") == 0) {
+				
 				int x, y;
 				if(scanf("%d%d", &x, &y));
 				rm(s, x, y);
@@ -26,7 +75,7 @@ int main() {
 				print(s);
 			}
 		else if (strcmp(command, "len") == 0) {
-				printf("%d\n", sz(s));
+				printf("%d\n", get_length(s));
 			}
 		else if (strcmp(command, "exit") == 0) {
 				clear(s);
