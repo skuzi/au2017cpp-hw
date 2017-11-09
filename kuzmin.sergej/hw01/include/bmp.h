71a5ba2c4adc279bcb85d4f6fee7b1224d5b9b27
kuzmin.sergej <kuzmin.sergej@a12908ea-3afd-459a-82ef-25d554c7e142>
2017-11-09 23:52:07 +0000

    update

--- kuzmin.sergej/hw01/include/bmp.h
+++ kuzmin.sergej/hw01/include/bmp.h
@@ -27,9 +27,9 @@ typedef struct tagBITMAPFILEHEADER {
 } BITMAPFILEHEADER;
 
 typedef struct tPixel{
-    uint8_t blue;
-    uint8_t green;
     uint8_t red;
+    uint8_t green;
+    uint8_t blue;
 } pixel;
 
 typedef struct bitmapImage {
