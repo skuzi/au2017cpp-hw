af6b402be91738ba163995ba9de7898b1287cf98
kuzmin.sergej <32015627+skuzi@users.noreply.github.com>
2017-11-30 22:54:26 +0000

    some big fixes such as initializations lists and simplification of isNull() function

--- lab09/src/shared_ptr.cpp
+++ lab09/src/shared_ptr.cpp
@@ -1,60 +1,44 @@
 #include "shared_ptr.h"
 #include "matrix.h"
-shared_ptr::shared_ptr(Matrix* obj) {
-	if(obj == NULL)
-		storage_ = NULL;
-	else
-		storage_ = new Storage(obj);
-}
-shared_ptr::shared_ptr(const shared_ptr& other) {
-	storage_ = other.storage_;
-	if(!isNull())
-		storage_->incr();
+shared_ptr::shared_ptr(Matrix* obj)
+	:storage_(new Storage(obj)){}
+
+shared_ptr::shared_ptr(const shared_ptr& other) 
+	:storage_(other.storage_)
+{
+	storage_->incr();
 }
 shared_ptr& shared_ptr::operator=(shared_ptr other) {
 	if(&other == this)
 		return *this;
-	if(!isNull()) {
-		storage_->decr();
-		if(!storage_->getCounter())
-			delete storage_;
-	}
-	if(!other.isNull()){
-		storage_ = other.storage_;
-		storage_->incr();
-    }
+	storage_->decr();
+	if(!storage_->getCounter())
+		delete storage_;
+
+	storage_ = other.storage_;
+	storage_->incr();
 	return *this;
 }
 shared_ptr::~shared_ptr() {
-	if(isNull())
-		return;
 	storage_->decr();
 	if(!storage_->getCounter())
 		delete storage_;
 }
 
 Matrix* shared_ptr::ptr() const {
-	if(isNull())
-		return NULL;
 	return storage_->getObject();
 }
 bool shared_ptr::isNull() const {
-	return !storage_;
+	return !storage_ || !storage_->getObject();
 }
 void shared_ptr::reset(Matrix* obj) {
-	if(isNull()) return;
 	storage_->decr();
 	if(!storage_->getCounter())
 		delete storage_;
-	if(obj != NULL)
-		storage_ = new Storage(obj);
-	else
-		storage_ = NULL;
+	storage_ = new Storage(obj);
 }
 
 Matrix* shared_ptr::operator->() const {
-	if(isNull())
-		return NULL;
 	return storage_->getObject();
 }
 Matrix& shared_ptr::operator*() const {
@@ -64,6 +48,7 @@ Matrix& shared_ptr::operator*() const {
   
 shared_ptr::Storage::Storage(Matrix* mtx):
 	data_(mtx), ref_count_(1){}
+
 shared_ptr::Storage::~Storage() {
 	delete data_;
 }
