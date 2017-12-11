ca404735facf23d99e60e15da58e2de6c3d22cb7
kuzmin.sergej <32015627+skuzi@users.noreply.github.com>
2017-12-11 18:14:26 +0000

    update hw02

--- hw02/src/main.cpp
+++ hw02/src/main.cpp
@@ -7,9 +7,9 @@
 int main(int argc, char *argv[]) {
     Board brd;
     View view(brd);
+    
     if(argc > 1 && !strcmp(argv[1], "silent"))
-        view.setSilent(1);
+        view.setSilent(true);
 
-    if(argc > 2 && freopen("ss", "r", stdin));
     view.doGameCycle();
 }
