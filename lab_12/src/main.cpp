df909586fc0ca723da7645fb4001ad81fb72c635
kuzmin.sergej <32015627+skuzi@users.noreply.github.com>
2018-03-11 23:33:17 +0000

    update lab_12

--- lab_12/src/main.cpp
+++ lab_12/src/main.cpp
@@ -15,10 +15,10 @@ public:
     Product():
     	name_(new char[8]), quantity_(0), price_(0)
     {
-    	strcpy(name_, "DEFNAME");
+    	strncpy(name_, "DEFNAME", 8);
     }
 
-    Product(Product& other):
+    Product(const Product& other):
     	name_(new char[strlen(other.name_) + 1]), quantity_(other.quantity_), price_(other.price_)
     {
     	strcpy(name_, other.name_);
@@ -58,19 +58,19 @@ void print(const my_vector<Product>& v) {
 
 template<class T>
 void test_my_vector(my_vector<T>& a) {
-	std::cout << a;
-	size_t t = a.size();
-	T c;
-	a.push_back(c);
+    size_t t = a.size();
+    a.push_back(T());
+    std::cout << a;
 	assert(a.size() == t + 1);
 	a.pop_back();
 	assert(a.size() == t);
 	a.clear();
 	assert(a.size() == 0);
 	assert(a.empty());
-	a.push_back(c);
+	a.push_back(T());
 	assert(a.size() == 1);
 	a.reserve(3);
+
 	assert(a.size() == 1);
 	assert(a.capacity() == 4);
 	a.reserve(2);
@@ -79,12 +79,42 @@ void test_my_vector(my_vector<T>& a) {
 	a.resize(3);
 	assert(a.size() == 3);
 	assert(a.capacity() == 4);
-	a.push_back(c);
+	a.push_back(T());
 	assert(a.size() == 4);
 	assert(a.capacity() == 4);
-	a.push_back(c);
+	a.push_back(T());
 	assert(a.size() == 5);
 	assert(a.capacity() == 8);
+    a.resize(50);
+    assert(a.capacity() == 64);
+    std::cout << a << std::endl;
+    a.resize(2);
+    assert(a.size() == 2);
+    assert(a.capacity() == 64);
+    std::cout << a << std::endl;
+    a.resize(0);
+    assert(a.empty());
+    assert(a.capacity() == 64);
+    std::cout << a << std::endl;
+    a.reserve(100);
+    assert(a.size() == 0);
+    assert(a.capacity() == 128);
+    a.reserve(80);
+    assert(a.capacity() == 128);
+    a.resize(50);
+    assert(a.size() == 50);
+    assert(a.capacity() == 128);
+    std::cout << a << std::endl;
+    my_vector<T> b(10);
+    std::cout << b << std::endl;
+    a = b;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    a.push_back(T());
+    assert(b.size() == 10);
+    assert(a.size() == 11);
+    assert(b.capacity() == 16);
+    assert(a.capacity() == 16);
 }
 
 int main() {
@@ -95,9 +125,8 @@ int main() {
     test_my_vector<int>(a);
     my_vector<Product> b;
     Product d, e("qwe", -1, 7.5);
+    b.push_back(Product("qwe", -1, 7.5));
     b.push_back(d);
-    e = d;
-    b.push_back(e);
     test_my_vector<Product>(b);
     
     return 0;
