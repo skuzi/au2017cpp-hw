9c2877fb1cb4ef171e619a70cf31292725052acf
kuzmin.sergej <kuzmin.sergej@a12908ea-3afd-459a-82ef-25d554c7e142>
2018-03-11 23:33:17 +0000

    update lab_12

--- kuzmin.sergej/lab_12/include/my_vector.h
+++ kuzmin.sergej/lab_12/include/my_vector.h
@@ -1,13 +1,14 @@
 #pragma once
 #include <cstddef>
 #include <iostream>
+#include <new>
 
 template <class T>
 class my_vector{
 public:
     my_vector();
     my_vector(size_t n);
-    my_vector(my_vector& other);
+    my_vector(const my_vector& other);
     my_vector& operator=(my_vector other);
     ~my_vector();
 
@@ -27,37 +28,47 @@ public:
     	return os;
     }
 
-    void push_back(T& t);
+    void push_back(const T& t);
     void pop_back();
     void clear();
 
 private:
     size_t size_;
     size_t capacity_;
-    T* array_;
+    int8_t* array_;
 };
+
+static int pow2(int n) {
+	int t = 1;
+	while(t < n)
+		t *= 2;
+	return t;		
+}
 
 template<class T>
 my_vector<T>::my_vector():
-	size_(0), capacity_(1)
-{		
-	array_ = new T[2];
+	size_(0), capacity_(1), array_(new int8_t[sizeof(T)]) {}
+
+template<class T>
+static void new_el(int8_t* array, size_t index, const T& el) {
+	new (array + index * sizeof(T)) T(el);
 }
 
 template<class T>
 my_vector<T>::my_vector(size_t n):
-	size_(0), capacity_(n)
+	size_(n), capacity_(pow2(n)), array_(new int8_t[size_ * sizeof(T)])
 {
-	array_ = new T[n];
+	for(size_t i = 0; i < size_; i++)
+		new_el(array_, i, T());
 }
 
+
 template<class T>
-my_vector<T>::my_vector(my_vector<T>& other):
-	size_(other.size_), capacity_(other.capacity_)
+my_vector<T>::my_vector(const my_vector<T>& other):
+	size_(other.size_), capacity_(other.capacity_), array_(new int8_t[capacity_ * sizeof(T)])
 {
-	array_ = new T[capacity_];
 	for(size_t i = 0; i < size_; i++)
-		array_[i] = other[i];
+		new_el(array_, i, other[i]);
 }
 
 template<class T>
@@ -79,13 +90,13 @@ void my_vector<T>::reserve(size_t n) {
 	if(n <= capacity_)
 		return;
 
-	while(capacity_ < n)
-		capacity_ *= 2;
-
-	T* tmp = new T[capacity_];
+	capacity_ = pow2(n);
+	int8_t* tmp = new int8_t[capacity_ * sizeof(T)];
 	for(size_t i = 0; i < size_; i++)
-		tmp[i] = array_[i];
+		new_el(tmp, i, (*this)[i]);
 	
+	for(size_t i = 0; i < size_; i++)
+		(*this)[i].~T();
 	delete [] array_;
 	array_ = tmp;
 }
@@ -95,29 +106,33 @@ void my_vector<T>::resize(size_t n) {
 	reserve(n);
 	if(n < size_)
 		for(size_t i = n; i < size_; i++)
-			array_[i].~T();
+			(*this)[i].~T();
+	else
+		for(size_t i = size_; i < n; i++)
+			new_el(array_, i, T());
 	size_ = n;
 }
 
 template<class T>
 T my_vector<T>::operator[](size_t index) const {
-	return array_[index];
+	return *((T*)(array_ + index * sizeof(T)));
 }
 
 template<class T>
 T& my_vector<T>::operator[](size_t index) {
-	return array_[index];
+	return *((T*)(array_ + index * sizeof(T)));
 }
 
 template<class T>
-void my_vector<T>::push_back(T& t) {
+void my_vector<T>::push_back(const T& t) {
 	reserve(size_ + 1);
-	array_[size_++] = t;
+	new_el(array_, size_++, t);
 }
 
 template<class T>
 void my_vector<T>::pop_back() {
 	size_--;
+	(*this)[size_].~T();
 }
 
 template<class T>
