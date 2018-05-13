2443fbc97daaad10f9ee317aa16ed22f57e6a6b9
kuzmin.sergej <32015627+skuzi@users.noreply.github.com>
2018-05-13 23:13:15 +0000

    update src

--- hw03/src/huffman.cpp
+++ hw03/src/huffman.cpp
@@ -1,17 +1,18 @@
 #include "huffman.h"
 #include <iostream>
 #include <fstream>
-#include <queue>
+#include <set>
 #include <deque>
 #include <cassert>
+#include <algorithm>
 
-static std::size_t frequency(shared_ptr<TreeNode> t) {
+static std::size_t frequency(std::shared_ptr<TreeNode> t) {
     if(t == nullptr)
         return 0;
     return t->get_freq();
 }
 
-TreeNode::TreeNode(char c, std::size_t f, shared_ptr<TreeNode> l, shared_ptr<TreeNode> r): left(l), right(r), symbol(c) {
+TreeNode::TreeNode(char c, std::size_t f, std::shared_ptr<TreeNode> l, std::shared_ptr<TreeNode> r): left(l), right(r), symbol(c) {
     freq = frequency(left) + frequency(right) + f;
 }
 
@@ -24,17 +25,24 @@ char TreeNode::get_symbol() const {
     return symbol;
 }
 
-shared_ptr<TreeNode> TreeNode::get_left() const {
+std::shared_ptr<TreeNode> TreeNode::get_left() const {
     return left;
 }
 
-shared_ptr<TreeNode> TreeNode::get_right() const {
+std::shared_ptr<TreeNode> TreeNode::get_right() const {
     return right;
 }
 
+bool TreeNode::is_terminal() const {
+    return terminal;
+}
 
-bool TreeNode::compare::operator()(shared_ptr<TreeNode> l, shared_ptr<TreeNode> r) {
-    return frequency(l) > frequency(r);
+void TreeNode::set_terminal(bool t) {
+    terminal = t;
+}
+
+bool TreeNode::compare::operator()(std::shared_ptr<TreeNode> l, std::shared_ptr<TreeNode> r) {
+    return frequency(l) < frequency(r);
 }
 
 static void make_freq(const std::string& s, std::map<char, std::size_t> &freq) {
@@ -59,6 +67,7 @@ static std::string bitvec_to_string(code_t bitvec) {
             byte = 0;
         }
     }
+
     if (nbits) {
         result += byte;
     }
@@ -69,11 +78,13 @@ static std::string bitvec_to_string(code_t bitvec) {
 static code_t string_to_bitvec(std::string s) {
     code_t result;
     unsigned nbits = s[0];
+
     for (std::string::iterator it = s.begin() + 1; it != s.end(); ++it) {
         for (unsigned i = 0; i < 8; i++) {
             result.push_back((*it >> i) & 1);
         }
     }
+
     if (nbits) {
         for (unsigned i = 0; i < (8 - nbits); i++) {
             result.pop_back();
@@ -82,64 +93,88 @@ static code_t string_to_bitvec(std::string s) {
     return result;
 }
 
+static void go(std::shared_ptr<TreeNode> t, code_t code, codetable &codes) {
+    if (t == nullptr)
+        return;
+    if (t->is_terminal())
+        codes[t->get_symbol()] = code;
+    code_t cpy(code);
+    cpy.push_back(1);
+    code.push_back(0);
+    go(t->get_left(), code, codes);
+    go(t->get_right(), cpy, codes);
+}
+
 void HuffmanTree::build_codes() {
-    std::deque<std::pair<shared_ptr<TreeNode>, code_t> > q;
-    q.push_back(std::make_pair(root, code_t()));
-    while (!q.empty()) {
-        shared_ptr<TreeNode> node = q.front().first, left_child, right_child;
-        code_t code = q.front().second;
-        q.pop_front();
-        if(node == nullptr)
-            continue;
-        left_child = node->left;
-        right_child = node->right;
-        if (left_child) {
-            code_t code_cp(code);
-            q.push_back({ left_child, (code.push_back(0), code) });
-            q.push_back({ right_child, (code_cp.push_back(1), code_cp) });
-        } else {
-            codes.insert(std::make_pair(node->get_symbol(), code));
-        }
-    }
+    go(root, code_t(), codes);
 }
 
 void HuffmanTree::decode(std::istream& in, std::ostream& out) {
     std::size_t info_useful = 0;
+    std::size_t file_sz = 0;
+    std::size_t info_additional = 0;
+
     in.read(reinterpret_cast<char*> (&info_useful), sizeof(std::size_t));
+    
     std::string data;
-    for(std::size_t i = 0; i <= info_useful; i++){
+    //std::cerr << "okay\n";
+    //std::cerr << "read info_useful " << info_useful << '\n';
+    for(std::size_t i = 0; i < info_useful; i++){
         char c = '\0';
         if(!in.read(&c, sizeof(char)))
         	throw std::logic_error("File corrupted");
         data.push_back(c);
+        //std::cerr << "read " << (int)c << '\n';
     }
+    
     std::size_t alph_sz = 0;
-    std::size_t info_additional = sizeof(std::size_t);
+    info_additional += sizeof(std::size_t);
+    
     in.read(reinterpret_cast<char*> (&alph_sz), sizeof(std::size_t));
-    for(std::size_t i = 0; i < alph_sz * sizeof(char); i++) {
+    //std::cerr << "alph_sz " << alph_sz << '\n';
+    for(std::size_t i = 0; i < alph_sz; i++) {
         char symbol = '\0';
         if(!in.read(&symbol, sizeof(char)))
         	throw std::logic_error("File corrupted");
+        std::cerr << "byte " << (int)symbol << '\n';
+        
         std::size_t frequency = 0;
         if(!in.read(reinterpret_cast<char*> (&frequency), sizeof(std::size_t)))
         	throw std::logic_error("File corrupted");
+        std::cerr << "freq " << frequency << '\n' << '\n';
         freq[symbol] = frequency;
+        
         info_additional += sizeof(char) + sizeof(std::size_t);
     }
 
     build_tree();
-    std::size_t file_sz = 0;
+    build_codes();
+    std::cerr << '\n';
+    for(auto t : codes) {
+        std::cerr << "byte " << (int)t.first << '\n' << "code ";
+        for(auto i : t.second)
+            std::cerr << i;
+        std::cerr << "\n\n";
+    }
+    std::cerr << "data" << '\n';
+    
     code_t compressed = string_to_bitvec(data);
-    shared_ptr<TreeNode> cur = root;
+    for(auto i : compressed)
+        std::cerr << i;
+    std::cerr << '\n';
+    std::shared_ptr<TreeNode> cur = root;
     for(std::size_t i = 0; i < compressed.size(); i++) {
         if(compressed[i] == 0)
             cur = cur->left;
         else
             cur = cur->right;
-        if(cur->get_symbol() != '\0') {
+        std::cerr << compressed[i];
+        if(cur->is_terminal()) {
             char c = cur->get_symbol();
             out.write(&c, sizeof(char));
+            std::cerr << '\n';
             file_sz++;
+            
             cur = root;
         }
     }
@@ -154,52 +189,70 @@ void HuffmanTree::encode(std::istream& in, std::ostream& out) {
         data.push_back(c);
         file_sz += sizeof(char);
     }
+
     make_freq(data, freq);
     build_tree();
     build_codes();
+    for(auto t : codes) {
+        std::cerr << "byte " << (int)t.first << '\n' << "code ";
+        for(auto i : t.second)
+            std::cerr << i;
+        std::cerr << "\n\n";
+    }
+    for(auto t : freq) {
+        std::cerr << "byte " << (int)t.first << '\n' << "freq " << t.second << "\n\n";
+    }
+    //if(root->right->get_symbol() == '\0')
+    //   throw std::logic_error("WTFFFFFF");
+    
     std::size_t info_useful = 0, info_additional = 0;
+    
     code_t result;
+    std::cerr<< "data\n";
     for(char c : data) {
+        for(auto t : codes[c])
+            std::cerr << t;
+        std::cerr << "\n";
         result.insert(result.end(), codes[c].begin(), codes[c].end());
     }
 
     std::string res = bitvec_to_string(result);
-    info_useful = (result.size() + 7) / 8;
+    std::cerr << "\ndata\n";
+    for(auto t : result)
+        std::cerr << t;
+    info_useful = res.size();
     out.write(reinterpret_cast<char*> (&info_useful), sizeof(std::size_t));
-    out.write(res.c_str(), info_useful + 1);
+    out.write(res.c_str(), info_useful * sizeof(char));
 
 
     std::size_t sz = freq.size();
     out.write(reinterpret_cast<char*> (&sz), sizeof(std::size_t));
     info_additional = sizeof(std::size_t);
+
     for(auto frequency : freq) {
         out.write(&frequency.first, sizeof(char));
         out.write(reinterpret_cast<char*> (&frequency.second), sizeof(std::size_t));
         info_additional += sizeof(char) + sizeof(std::size_t);
     }
+
     std::cout << file_sz << '\n' << info_useful << '\n' << info_additional << '\n';
 }
 
-
 void HuffmanTree::build_tree() {
-    std::priority_queue<shared_ptr<TreeNode>, std::vector<shared_ptr<TreeNode>>, TreeNode::compare> heap;
+    std::multiset<std::shared_ptr<TreeNode>, TreeNode::compare> heap;
     for(auto leaf : freq) {
         auto node = std::make_shared<TreeNode>(TreeNode(leaf.first, leaf.second));
-        heap.push(node);
+        heap.insert(node);
+        node->set_terminal(1);
     }
     root = nullptr;
-    if(heap.size() == 1) {
-        auto left = std::make_shared<TreeNode>(TreeNode(heap.top()->get_symbol(), heap.top()->get_freq()));
-        root = std::make_shared<TreeNode>(TreeNode(0, 0, left, nullptr));
+    
+    while (heap.size() > 2) {
+        auto left = *heap.begin();
+        heap.erase(heap.begin());
+        auto right = *heap.begin();
+        heap.erase(heap.begin());
+        heap.insert(root = std::make_shared<TreeNode>('\0', 0, left, right));
     }
-    else {
-        while(heap.size() > 1) {
-            shared_ptr<TreeNode> l(heap.top());
-            heap.pop();
-            shared_ptr<TreeNode> r(heap.top());
-            heap.pop();
-            root = std::make_shared<TreeNode>(TreeNode(0, 0, l, r));
-            heap.push(root);
-        }
-    }
+    root = std::make_shared<TreeNode>('\0', 0, *heap.begin(), (heap.size() <= 1 ? nullptr : *std::next(heap.begin())));
 }
