39793c8a0a2ba80f1aaeff7c5246bac7b3a78e75
kuzmin.sergej <kuzmin.sergej@a12908ea-3afd-459a-82ef-25d554c7e142>
2018-05-13 23:13:15 +0000

    update src

--- kuzmin.sergej/hw03/include/huffman.h
+++ kuzmin.sergej/hw03/include/huffman.h
@@ -3,24 +3,26 @@
 #include <map>
 #include <memory>
 #include <vector>
-using std::shared_ptr;
 
 typedef std::vector<bool> code_t;
 typedef std::map<char, code_t> codetable;
 class TreeNode {
 friend class HuffmanTree;
 public:
-    TreeNode(char c = '\0', std::size_t freq = 0, shared_ptr<TreeNode> l = nullptr, shared_ptr<TreeNode> r = nullptr);
+    TreeNode(char c = '\0', std::size_t freq = 0, std::shared_ptr<TreeNode> l = nullptr, std::shared_ptr<TreeNode> r = nullptr);
     std::size_t get_freq() const;
     char get_symbol() const;
-    shared_ptr<TreeNode> get_left() const;
-    shared_ptr<TreeNode> get_right() const;
+    std::shared_ptr<TreeNode> get_left() const;
+    std::shared_ptr<TreeNode> get_right() const;
+    bool is_terminal() const;
+    void set_terminal(bool);
 private:
     struct compare {
-        bool operator()(shared_ptr<TreeNode> left, shared_ptr<TreeNode> right);
+        bool operator()(std::shared_ptr<TreeNode> left, std::shared_ptr<TreeNode> right);
     };
-    shared_ptr<TreeNode> left, right;
+    std::shared_ptr<TreeNode> left, right;
     char symbol = '\0';
+    bool terminal = 0;
     std::size_t freq = 0;
 };
 
@@ -32,7 +34,7 @@ public:
     void decode(std::istream&, std::ostream&);
 private:
     std::map<char, std::size_t> freq;
-    shared_ptr<TreeNode> root = nullptr;
+    std::shared_ptr<TreeNode> root = nullptr;
     std::string data;
     codetable codes;
 };
